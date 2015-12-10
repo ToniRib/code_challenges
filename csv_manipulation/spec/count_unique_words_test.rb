670d6bcc4d1947dc6a0fc79ae13f0cc1838f8c77
ToniRib <tonimarierib@gmail.com>
2015-12-09 19:59:14 -0700

    test: Adds a few more tests to count unique words

--- csv_manipulation/spec/count_unique_words_test.rb
+++ csv_manipulation/spec/count_unique_words_test.rb
@@ -7,6 +7,13 @@ class CountUniqueWordsTest < Minitest::Test
     assert CountUniqueWords
   end
 
+  def test_counts_unique_words_in_an_array_with_no_multiples
+    counter = CountUniqueWords.new
+    a = %w(kitty cat meow purr fluff)
+
+    assert_equal 5, counter.unique(a)
+  end
+
   def test_counts_unique_words_in_an_array
     counter = CountUniqueWords.new
     a = %w(kitty cat meow meow kitty purr fluff)
@@ -14,21 +21,24 @@ class CountUniqueWordsTest < Minitest::Test
     assert_equal 5, counter.unique(a)
   end
 
+  def test_counts_unique_words_in_a_different_array
+    counter = CountUniqueWords.new
+    a = %w(kitty kitty meow meow kitty purr)
+
+    assert_equal 3, counter.unique(a)
+  end
+
   def test_counts_unique_words_in_small_file
     counter = CountUniqueWords.new
     filepath = './swift_stats/short_lyrics.txt'
 
-    num = counter.count(filepath)
-
-    assert_equal 20, num
+    assert_equal 20, counter.count(filepath)
   end
 
   def test_counts_unique_words_in_lyrics_file
     counter = CountUniqueWords.new
     filepath = './swift_stats/lyrics.txt'
 
-    num = counter.count(filepath)
-
-    assert_equal 1984, num
+    assert_equal 1984, counter.count(filepath)
   end
 end
